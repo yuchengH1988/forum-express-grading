d92a9d26b430dcb0afa5773b7eabef46f17675fb
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-04-06 07:18:11 +0800

    feat: init project

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
