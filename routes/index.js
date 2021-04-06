5b9831fc2223e86be67983ae7c009bf9a5b9aa4b
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-04-06 11:15:05 +0800

    install passport, passport-local, add feature: signinPage, signIn, logout

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,18 @@
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
-module.exports = app => {
+module.exports = (app, passport) => {
 
-  //如果使用者訪問首頁，就導向 /restaurants 的頁面
+
   app.get('/', (req, res) => res.redirect('/restaurants'))
-
-  //在 /restaurants 底下則交給 restController.getRestaurants 來處理
   app.get('/restaurants', restController.getRestaurants)
 
-  // 連到 /admin 頁面就轉到 /admin/restaurants
   app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
-
-  // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
   app.get('/admin/restaurants', adminController.getRestaurants)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
+  app.get('/signin', userController.signInPage)
+  app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+  app.get('/logout', userController.logout)
 }
