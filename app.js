76955be31ca4994c9d5b6c1708a1fd3bbf21fc5f
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-04-06 10:46:42 +0800

    install express-session connect-flash, add flash msgs

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const db = require('./models')
 const app = express()
 const port = 3000
@@ -8,6 +10,13 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
