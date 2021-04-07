833377704e12ec76dc03419a6ec340eeb30d5cff
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-04-07 14:46:32 +0800

    heroku setting

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require('method-override')
 const passport = require('./config/passport')
 const db = require('./models')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
