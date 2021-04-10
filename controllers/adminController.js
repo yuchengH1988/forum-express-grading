14316305f8cd1f16e7328decf26b2e4d25332106
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-04-10 20:26:51 +0800

    show category name one admin/ restaurants

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const db = require('../models')
 const Restaurant = db.Restaurant
 const User = db.User
+const Category = db.Category
 const userController = require('./userController')
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
@@ -9,7 +10,11 @@ const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({ raw: true }).then(restaurants => {
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    }).then(restaurants => {
       return res.render('admin/restaurants', { restaurants: restaurants })
     })
   },
