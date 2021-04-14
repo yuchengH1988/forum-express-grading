d3132f0d3e2e6075fc545dd855c046e5dbc6f1e0
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-04-14 18:06:52 +0800

    refactor adminController.getRestaurants

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,17 +5,14 @@ const User = db.User
 const Category = db.Category
 const userController = require('./userController')
 const imgur = require('imgur-node-api')
+const adminService = require('../services/adminService.js')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      raw: true,
-      nest: true,
-      include: [Category]
-    }).then(restaurants => {
-      return res.render('admin/restaurants', { restaurants: restaurants })
+    adminService.getRestaurants(req, res, (data) => {
+      return res.render('admin/restaurants', data)
     })
   },
   createRestaurant: (req, res) => {
@@ -156,4 +153,5 @@ const adminController = {
       })
   }
 }
+
 module.exports = adminController
---
+++ services/adminService.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const db = require('../models')
+const Restaurant = db.Restaurant
+const User = db.User
+const Category = db.Category
+
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+
+const adminService = {
+  getRestaurants: (req, res, callback) => {
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    }).then(restaurants => {
+      callback({ restaurants })
+    })
+  },
+}
+
+
+
+module.exports = adminService
